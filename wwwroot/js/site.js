64e8628f42c341cde56c44950653222ac415ea5d
yhl234 <43277189+yhl234@users.noreply.github.com>
2020-03-05 16:46:24 -0500

    show editor in projects create working on editor.save()

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,33 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
 // Write your JavaScript code.
+
+
+const editor = new EditorJS({
+    /** 
+     * Id of Element that should contain the Editor 
+     */
+    holderId: 'codex-editor',
+    tools: {
+        header: {
+            class: Header,
+            inlineToolbar: ['link']
+        },
+        list: {
+            class: List,
+            inlineToolbar: true
+        }
+    }, 
+})
+const bodyContent = document.querySelector('#BodyContent');
+const saveBtn = document.querySelector('#cdx-save');
+saveBtn.addEventListener('click', () => {
+    editor.save().then((outputData) => {
+        console.log('Article data: ', outputData)
+        bodyContent.value = outputData;
+        console.log('body: ', bodyContent.value)
+
+    }).catch((error) => {
+        console.log('Saving failed: ', error)
+    });
+})
