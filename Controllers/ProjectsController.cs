8d28d9c2aee874cbc997f7c2988afa12a6b30582
Ryan-Gee <43253067+Ryan-Gee@users.noreply.github.com>
2020-04-12 15:47:03 -0400

    upload testing

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -185,8 +185,7 @@ namespace onni.Controllers
 		[HttpPost]
 		public async Task<string> UploadImg(IFormFile file)
 		{
-			//var uploads = Path.Combine(hostingEnvironment.WebRootPath, "upload/img");
-			var uploads = $"wwwroot\\upload\\img\\";
+			var uploads = Path.Combine(hostingEnvironment.WebRootPath, "upload/img");
 			if (file.Length > 0)
 			{
 				var f = file.FileName;
